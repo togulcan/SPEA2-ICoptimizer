e7f50047c96852e1213ddfcb0ae4c0ab4275de3b
ogulcan <togulcancakici@gmail.com>
2020-08-04 22:01:00 +0300

    fitness data structure added

---
+++ src/spea2/fitness.py
@@ -0,0 +1,14 @@
+class Fitness:
+
+    __slots__ = ("total_error", "strength", "rawfitness",
+                 "distance", "fitness")
+
+    def __init__(self, N: int):
+        self.total_error = 0.0
+        self.strength = 0
+        self.rawfitness = 0
+        self.distance = [0.0] * N
+        self.fitness = 0.0
+
+    def __repr__(self):
+        return f"Fitness(error:{self.total_error}, rawfitness:{self.rawfitness})"
