b1b3b5178bbab7dbf4a1045cb94e6bed7124dfb9
ogulcan <togulcancakici@gmail.com>
2020-08-20 22:27:13 +0300

    refactored

--- src/spea2/helperfuncs.py
+++ src/spea2/helperfuncs.py
@@ -25,19 +25,17 @@ def get_normalize_constants(inds, archive_inds = None):
     archive_inds = [] if archive_inds is None else archive_inds
 
     return [max(ind.targets[j] for ind in inds + archive_inds)
-            for j in range(len(inds[0]._TARGETS))]
+            for j in range(len(inds[0].TARGETS))]
 
 
 def calculate_distance(first_ind, second_ind,
                        normalize_values: List[float]) -> float:
     """ Calculate the euclidian distance between two individuals."""
-
     dist = 0.0
     for i, tars in enumerate(zip(
             first_ind.targets, second_ind.targets)):
         dist += ((tars[0] - tars[1]) / normalize_values[i]) ** 2
-
-    dist = dist.sqrt()
+    dist = math.sqrt(dist)
     return dist
 
 
@@ -57,14 +55,13 @@ def calculate_fitness_value(fitness,
                             normalize_rawfitness: int,
                             kii: int) -> float:
     """ Calculates and returns final fitness values of each individuals"""
-
     if normalize_rawfitness != 0:
         fit = fitness.rawfitness / normalize_rawfitness \
-            + fitness.total_error * (20 + kii ** 4) * (1e-8)\
-            + (0.1) / (fitness.distance + (2))
+            + fitness.total_error * (20 + kii ** 4) * 1e-8\
+            + 0.1 / (fitness.distance + 2)
         return fit
     else:
-        fit = fitness.total_error * (20 + kii ** 4) * (1e-8) \
+        fit = fitness.total_error * (20 + kii ** 4) * 1e-8 \
             + 0.1 / (fitness.distance + 2)
         return fit
 
@@ -75,7 +72,7 @@ def calculate_total_error(ind) -> float:
     of the individuals exceeds constraint limits.
     """
     total_error = 0.0
-    for i in range(len(ind._CONSTRAINTS)):
+    for i in range(len(ind.CONSTRAINTS)):
         if ind.constraint_operations[i] == 'max':
             if ind.constraint_values[i] > ind.constraint_constants[i]:
                 total_error += abs(
