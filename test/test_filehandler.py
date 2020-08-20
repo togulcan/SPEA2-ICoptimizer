617c771b2f2281c547aa870ceaa87c5001c70cf9
ogulcan <togulcancakici@gmail.com>
2020-08-20 22:24:26 +0300

    directions changed

--- test/test_filehandler.py
+++ test/test_filehandler.py
@@ -1,15 +1,14 @@
 import pytest
 import os
 
-from docs.src import FileHandler
+from src import FileHandler
 
 
 def test_check_required_files():
-    path = 'circuitfiles/'
-    circuit_name = 'amp'
-    file_hand = FileHandler(path, circuit_name)
+    path = '../circuitfiles/amp/'
+    file_hand = FileHandler(path)
     file_hand.check_required_files(4)
-    assert os.path.isdir('circuitfiles/amp_temp/0')
-    assert os.path.isdir('circuitfiles/amp_temp/1')
-    assert os.path.isdir('circuitfiles/amp_temp/2')
-    assert os.path.isdir('circuitfiles/amp_temp/3')
+    assert os.path.isdir('../circuitfiles/amp_temp/0')
+    assert os.path.isdir('../circuitfiles/amp_temp/1')
+    assert os.path.isdir('../circuitfiles/amp_temp/2')
+    assert os.path.isdir('../circuitfiles/amp_temp/3')
