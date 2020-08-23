0e815ac41060640493959f386f2922b9d83707d2
ogulcan <togulcancakici@gmail.com>
2020-08-23 18:08:45 +0300

    method changed

--- test/test_filehandler.py
+++ test/test_filehandler.py
@@ -7,7 +7,7 @@ from src import FileHandler
 def test_check_required_files():
     path = '../circuitfiles/amp/'
     file_hand = FileHandler(path)
-    file_hand.check_required_files(4)
+    file_hand.form_simulation_environment(4)
     assert os.path.isdir('../circuitfiles/amp_temp/0')
     assert os.path.isdir('../circuitfiles/amp_temp/1')
     assert os.path.isdir('../circuitfiles/amp_temp/2')
