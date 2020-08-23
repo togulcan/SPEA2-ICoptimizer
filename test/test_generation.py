4a90c7dacd4be0c33eb201bd8754e916949a6693
ogulcan <togulcancakici@gmail.com>
2020-08-23 18:08:36 +0300

    paths changed

--- test/test_generation.py
+++ test/test_generation.py
@@ -5,13 +5,13 @@ from src.filehandler import FileHandler
 
 
 def test_simulate():
-    path = "C:\\Users\\ogulc\\Desktop\\MOO3\\circuitfiles\\comparator\\"
+    path = "C:..\\circuitfiles\\comparator\\"
     gen = Generation(600, 0)
     gen.population_initialize("Random")
     file_handler = FileHandler(path)
-    file_handler.check_required_files(8)
+    file_handler.form_simulation_environment(8)
 
-    path = "C:\\Users\\ogulc\\Desktop\\MOO3\\circuitfiles\\comparator_temp\\"
+    path = "..\\circuitfiles\\comparator_temp\\"
     gen.simulate(path, multithread=4)
     for ind in gen.individuals:
         assert ind.status == 'simulated'
